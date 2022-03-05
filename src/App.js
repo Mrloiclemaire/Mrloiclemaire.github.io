1bd8c51e403e81425fdfd611044a9720677cf732
Agathe Geniteau <geniteau@lemonde.fr>
2022-03-05 17:37:27 +0100

    add contact component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Map from './components/Map';
 import { useState } from "react";
 import { useEffect } from "react";
 import axios from "axios";
+import Contact from "./components/Contact"
+import Loic from "./images/loic.jpeg";
+
 
 const App=()=> {
 
@@ -28,6 +31,27 @@ const App=()=> {
 
   return (
     <div className="App">
+      <Contact
+        img={Loic}
+        name="Loïc Lemaire"
+        linkedinurl="https://www.linkedin.com/in/mrlo%C3%AFclemaire/"
+        linkedinname="linkedin/loiclemaire"
+        email="loic@gmail.com"
+      />
+      <Contact
+        img={Loic}
+        name="Alexandre Kermarec"
+        linkedinurl="https://www.linkedin.com/in/mrlo%C3%AFclemaire/"
+        linkedinname="linkedin/alexandrekermarec"
+        email="alexandre@gmail.com"
+      />
+       <Contact
+        img={Loic}
+        name="Agathe Geniteau"
+        linkedinurl="https://www.linkedin.com/in/mrlo%C3%AFclemaire/"
+        linkedinname="linkedin/agathegeniteau"
+        email="agathe@gmail.com"
+      />
       <Map flights={data}/>
     </div>
   );
